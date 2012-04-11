3cd288c496b9b178d7312de599804e7325b6da69
Sam Fentress <sfentress@concord.org>
2012-04-11 11:58:06 -0400

    Start of genetics library

---
+++ js/genetics.js
@@ -0,0 +1,48 @@
+(function() {
+
+  GG.genetics = {
+    allAlleles: {
+      tail: ['T', 'Tk', 't'],
+      metalic: ['M', 'm'],
+      wings: ['W', 'w'],
+      horns: ['H', 'h'],
+      color: ['C', 'c'],
+      forelimbs: ['Fl', 'fl'],
+      hindlimbs: ['Hl', 'hl'],
+      armor: ['A1', 'A2', 'a'],
+      black: ['B', 'b'],
+      dilute: ['D', 'd', 'dl'],
+      nose: ['Rh', 'rh']
+    },
+    alleleLabelMap: {
+      'T': 'Long tail',
+      'Tk': 'Kinked tail',
+      't': 'Short tail',
+      'M': 'Metallic',
+      'm': 'Nonmetallic',
+      'W': 'Wings',
+      'w': 'No wings',
+      'H': 'No horns',
+      'h': 'Horns',
+      'C': 'Colored',
+      'c': 'Colorless',
+      'Fl': 'Forelimbs',
+      'fl': 'No forelimbs',
+      'Hl': 'Hindlimbs',
+      'hl': 'No hindlimbs',
+      'A1': "'A1' armor",
+      'A2': "'A2' armor",
+      'a': "'a' armor",
+      'B': 'Black',
+      'b': 'Brown',
+      'D': 'Full color',
+      'd': 'Dilute color',
+      'dl': 'dl',
+      'Rh': 'Nose spike',
+      'rh': 'No nose spike'
+    },
+    'Y': 'Y',
+    '': ''
+  };
+
+}).call(this);
